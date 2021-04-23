13e698b95f02fd1f77b96b83700dd3b07f97241c
Neeraj Gupta <40573087+theneerajgupta@users.noreply.github.com>
2021-04-24 02:36:11 +0530

    Update 04 Preprocess and Split Dataset into Training and Main.py

--- Python Files/04 Preprocess and Split Dataset into Training and Main.py
+++ Python Files/04 Preprocess and Split Dataset into Training and Main.py
@@ -51,12 +51,16 @@ def remove_tags(text):
     TAG_RE = re.compile(r'<[^>]+>')
     return TAG_RE.sub('', text)
 
-stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"]
+stopwords = set(["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"])
 
 def remove_single_chars(text) :
     array = text.split()
     return (" ".join([w for w in array if len(w) > 1]))
 
+def remove_stopwords(text) :
+    text = " ".join([word for word in text.split() if word not in stopwords])
+    return text
+    
 def preprocess_text(sen) :
     sentence = remove_tags(sen)
     sentence = re.sub('@[A-Za-z]+[A-Za-z0-9-_]+', '', sentence)
@@ -83,8 +87,8 @@ df_list = df.values.tolist()
 df_main = df_list[:10000] + df_list[800000:810000]
 df_train = df_list[10000:800000] + df_list[810000:]
 
-final_main = pd.DataFrame(df_main, columns=['USER', 'TEXT', 'SENTIMENT'])
-final_train = pd.DataFrame(df_train, columns=['USER', 'TEXT', 'SENTIMENT'])
+main = pd.DataFrame(df_main, columns=['USER', 'TEXT', 'SENTIMENT'])
+train = pd.DataFrame(df_train, columns=['USER', 'TEXT', 'SENTIMENT'])
 
-final_main.to_csv('db/04-main-data.csv', index=None)
-final_train.to_csv('db/04-train-data.csv', index=None)
+main.to_csv('db/04-main-data.csv', index=None)
+train.to_csv('db/04-train-data.csv', index=None)
